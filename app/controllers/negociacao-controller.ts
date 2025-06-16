46e5ffdf5171f3ce7c62697d0bcf44a4a8584032
BRUNO SANTOS <bhas795002@gmail.com>
2025-06-16 09:14:21 -0300

    tornando impossivel modeificar os objetos da lista

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
     private inputData:HTMLInputElement ;
     private inputQuantidade : HTMLInputElement;
     private inputValor : HTMLInputElement;
+    private negociacoes = new Negociacoes();
 
     constructor(){
         this.inputData = document.querySelector('#data');
@@ -11,9 +13,13 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor');
     }
 
-    adiciona(){
+    adiciona():void{
         const negociacao = this.criaNegociacao();
+        this.negociacoes.lista();
+        this.negociacoes.adiciona(negociacao);
+        console.log(this.negociacoes.lista())
         console.log(negociacao);
+        this.limparFormulario();
     }
 
     criaNegociacao(): Negociacao// isso aqui faz ele o ts já saber o que você quer retornar
@@ -22,6 +28,12 @@ export class NegociacaoController {
             const date = new Date(this.inputData.value.replace(exp, ','));
             const quantidade = parseInt(this.inputQuantidade.value);
             const valor = parseFloat(this.inputValor.value);
-           return new Negociacao(date, quantidade, valor);
+            return new Negociacao(date, quantidade, valor);
+    }
+    limparFormulario():void{
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
     }
 }
